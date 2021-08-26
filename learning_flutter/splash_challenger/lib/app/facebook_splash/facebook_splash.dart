41e9abf18ae97e8ea33a6540d241225385bc9727
alvdelci <delcimariosantos@gmail.com>
2021-08-25 23:06:15 -0300

    Corrigindo ícone principal

---
+++ learning_flutter/splash_challenger/lib/app/facebook_splash/facebook_splash.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:splash_challenger/app/whatsapp_splash/whatsapp_splash.dart';
+
+class FacebookSplash extends StatefulWidget {
+  @override
+  _FacebookSplashState createState() => _FacebookSplashState();
+}
+
+class _FacebookSplashState extends State<FacebookSplash> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        brightness: Brightness.light, // status bar brightness
+      ),
+      backgroundColor: Colors.lightBlue[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 60,
+              width: 60,
+              padding: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    image: AssetImage('assets/image/fb_icon.png'),
+                    fit: BoxFit.fill),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 5,
+                  height: 5,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 5,
+                  height: 5,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 5,
+                  height: 5,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10),
+                Container(width: 5, height: 5, color: Colors.blue[700]),
+                SizedBox(width: 10),
+                Container(
+                  width: 5,
+                  height: 5,
+                  color: Colors.blue[700],
+                ),
+              ],
+            ),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        
+        backgroundColor: Colors.transparent,
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => WhatsappSplash())),
+        child: Image.asset('assets/image/wpp_icon.png'),
+      ),
+    );
+  }
+}
