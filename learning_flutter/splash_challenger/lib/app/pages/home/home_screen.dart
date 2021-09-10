0e0b9c7983a590e43599fbe0d6c20912cc922fff
alvdelci <delcimariosantos@gmail.com>
2021-09-09 23:39:10 -0300

    Removendo backgroundColor e navegação para InstagramProfile

--- learning_flutter/splash_challenger/lib/app/pages/home/home_screen.dart
+++ learning_flutter/splash_challenger/lib/app/pages/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splash_challenger/app/pages/facebook_splash/facebook_splash.dart';
+import 'package:splash_challenger/app/pages/instagram_profile/instagram_profile.dart';
 import 'package:splash_challenger/app/pages/tinder_splash/tinder_splash.dart';
 import 'package:splash_challenger/app/pages/whatsapp_splash/whatsapp_splash.dart';
 
@@ -13,10 +14,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   List<Widget> _screenList = <Widget>[
-
     FacebookSplash(),
     TinderSplash(),
-    WhatsappSplash()
+    WhatsappSplash(),
+    InstagramProfile()
   ];
 
   void _itemTapped(int index) {
@@ -28,42 +29,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
         height: double.maxFinite,
         width: double.maxFinite,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Colors.orange[600],
-          Colors.orange,
-          Colors.red,
-          Colors.pink,
-          Colors.pink[600]
-        ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
         child: Center(child: _screenList.elementAt((_selectedIndex))),
       ),
       bottomNavigationBar: BottomNavigationBar(
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
               icon: Image.asset('assets/image/fb_icon2.png',
                   height: 30, width: 30),
-              label: 'Facebook',
-              backgroundColor: Colors.blue),
+              label: 'Facebook'),
           BottomNavigationBarItem(
-              icon: Image.asset('assets/image/tinder_icon.png',
-                  height: 30, width: 30),
-              label: 'Tinder',
-              backgroundColor: Colors.orange),
+            icon: Image.asset('assets/image/tinder_icon.png',
+                height: 30, width: 30),
+            label: 'Tinder',
+          ),
           BottomNavigationBarItem(
-              icon: Image.asset('assets/image/wpp_icon.png',
-                  height: 30, width: 30),
-              label: 'Whatsapp',
-              backgroundColor: Colors.green),
+            icon:
+                Image.asset('assets/image/wpp_icon.png', height: 30, width: 30),
+            label: 'Whatsapp',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/ig_icons/ig_icon.png",
+              height: 27,
+              width: 27,
+            ),
+            label: 'Instagram',
+          )
         ],
         currentIndex: _selectedIndex,
         selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        selectedItemColor: _selectedIndex == 0 ? Colors.blue : _selectedIndex == 1 ? Colors.orange : Colors.green,
+        selectedItemColor: _selectedIndex == 0
+            ? Colors.blue
+            : _selectedIndex == 1
+                ? Colors.orange
+                : _selectedIndex == 2
+                    ? Colors.green
+                    : Colors.pink,
         onTap: _itemTapped,
       ),
     );
